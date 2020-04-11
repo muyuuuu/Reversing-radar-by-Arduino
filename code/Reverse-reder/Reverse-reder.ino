fa3868b191947b062a3212f34b09153568426d89
muyuuuu <muyu0223@outlook.com>
2020-04-11 19:12:36 +0800

    finish L298N

--- code/Reverse-reder/Reverse-reder.ino
+++ code/Reverse-reder/Reverse-reder.ino
@@ -1,13 +1,33 @@
-// 后方超声波 **************************************
+// 后方超声波 ------------------------------------
 int trigBack = 13;
 int echoBack = A0;
-// 右侧超声波 **************************************
+// 右侧超声波 ------------------------------------
 int trigRight = 8;
 int echoRight = A1;
-// 左侧超声波 **************************************
+// 左侧超声波 ------------------------------------
 int trigLeft = 2;
 int echoLeft = A2;
-// 设置蓝牙的连接 **********************************
+// 右前轮 -----------------------------------------
+// 定义 uno 的 pin 11 向 input1 输出
+int input1 = 9; 
+// 定义 uno 的 pin 12 向 input2 输出
+int input2 = 10; 
+// 左前轮 -----------------------------------------
+// 定义 uno 的 pin 9 向 input3 输出
+int input3 = 11; 
+// 定义 uno 的 pin 10 向 input4 输出
+int input4 = 12; 
+// 右后轮 -----------------------------------------
+// 定义 uno 的 pin 11 向 input1 输出
+int input5 = 3; 
+// 定义 uno 的 pin 12 向 input2 输出
+int input6 = 4; 
+// 左后轮 -----------------------------------------
+// 定义 uno 的 pin 9 向 input3 输出
+int input7 = 5; 
+// 定义 uno 的 pin 10 向 input4 输出
+int input8 = 6; 
+// 设置蓝牙的连接 -----------------------------------
 String str = "";
 // 后侧超声波
 long cmB, cmR, cmL;
@@ -15,20 +35,120 @@ long cmB, cmR, cmL;
 void setup() {
 	// 设置硬件串口通信的波特率
 	Serial.begin(9600);
-	// 定义后侧超声波的输入输出模式 *******************
+	// 定义后侧超声波的输入输出模式 -----------------------------
 	pinMode(trigBack, OUTPUT);
 	pinMode(echoBack, INPUT);
-	// 定义右侧超声波的输入输出模式 *******************
+	// 定义右侧超声波的输入输出模式 -----------------------------
   	pinMode(trigRight, OUTPUT);
   	pinMode(echoRight, INPUT); 
-	// 定义左侧超声波的输入输出模式 *******************
+	// 定义左侧超声波的输入输出模式 -----------------------------
   	pinMode(trigLeft, OUTPUT);
+	  
   	pinMode(echoLeft, INPUT); 	  
+	// 定义所有的电机均为输出模式 ------------------------------
+	// 右前
+	pinMode(input1,OUTPUT);
+	pinMode(input2,OUTPUT);
+	// 左前  
+	pinMode(input3,OUTPUT);
+	pinMode(input4,OUTPUT);
+	// 右后
+	pinMode(input5,OUTPUT);
+	pinMode(input6,OUTPUT);
+	// 左后  
+	pinMode(input7,OUTPUT);
+	pinMode(input8,OUTPUT);
 }
- 
+
+// 前进函数 ------------------------------------------
+void forward()
+{
+	// 左前轮
+	digitalWrite(input1, LOW); 
+	digitalWrite(input2, HIGH);  
+	// 右前轮 
+	digitalWrite(input3, LOW); 
+	digitalWrite(input4, HIGH);
+	// 右后轮
+	digitalWrite(input5, HIGH);
+	digitalWrite(input6, LOW);
+	// 左后轮
+	digitalWrite(input7, HIGH);
+	digitalWrite(input8, LOW);
+}
+
+// 停止函数 ------------------------------------------
+void stop()
+{
+	// 左前轮
+	digitalWrite(input1, LOW); 
+	digitalWrite(input2, LOW);  
+	// 右前轮 
+	digitalWrite(input3, LOW); 
+	digitalWrite(input4, LOW);
+	// 右后轮
+	digitalWrite(input5, LOW);
+	digitalWrite(input6, LOW);
+	// 左后轮
+	digitalWrite(input7, LOW);
+	digitalWrite(input8, LOW);
+}
+
+// 后退函数 ------------------------------------------
+void back()
+{
+	// 左前轮
+	digitalWrite(input1, HIGH); 
+	digitalWrite(input2, LOW);  
+	// 右前轮 
+	digitalWrite(input3, HIGH); 
+	digitalWrite(input4, LOW);
+	// 右后轮
+	digitalWrite(input5, LOW);
+	digitalWrite(input6, HIGH);
+	// 左后轮
+	digitalWrite(input7, LOW);
+	digitalWrite(input8, HIGH);
+}
+
+// 左转函数 ------------------------------------------
+void left()
+{
+	// 左前轮
+	digitalWrite(input1, LOW); 
+	digitalWrite(input2, HIGH);  
+	// 左后轮
+	digitalWrite(input7, HIGH);
+	digitalWrite(input8, LOW);
+	// 右前轮 
+	digitalWrite(input3, HIGH); 
+	digitalWrite(input4, LOW);
+	// 右后轮
+	digitalWrite(input5, LOW);
+	digitalWrite(input6, HIGH);
+}
+
+// 右转函数 ------------------------------------------
+void right()
+{
+	// 右前轮 
+	digitalWrite(input3, LOW); 
+	digitalWrite(input4, HIGH);
+	// 右后轮
+	digitalWrite(input5, HIGH);
+	digitalWrite(input6, LOW);
+	// 左前轮
+	digitalWrite(input1, HIGH); 
+	digitalWrite(input2, LOW);  
+	// 左后轮
+	digitalWrite(input7, LOW);
+	digitalWrite(input8, HIGH);
+}
+
 void loop()
 {
-	// 后方超声波开启 **********************************************
+	forward();
+	// 后方超声波开启 --------------------------------------------
 	digitalWrite(trigBack, LOW);
 	delayMicroseconds(5);
 	digitalWrite(trigBack, HIGH);
@@ -37,7 +157,7 @@ void loop()
 	// 计算后方超声波的距离
 	cmB = (pulseIn(echoBack, HIGH) / 2) / 29.1;
 
-	// 右侧超声波开启 **********************************************
+	// 右侧超声波开启 --------------------------------------------
 	digitalWrite(trigRight, LOW);
 	delayMicroseconds(5);
 	digitalWrite(trigRight, HIGH);
@@ -46,7 +166,7 @@ void loop()
 	// 计算后方超声波的距离
 	cmR = (pulseIn(echoRight, HIGH) / 2) / 29.1;
 
-	// 左侧超声波开启 **********************************************
+	// 左侧超声波开启 --------------------------------------------
 	digitalWrite(trigLeft, LOW);
 	delayMicroseconds(5);
 	digitalWrite(trigLeft, HIGH);
--- code/test_vscode/test_vscode.ino
+++ code/test_vscode/test_vscode.ino
@@ -1,14 +1,17 @@
-#include <SoftwareSerial.h>
-// 设置软串口使用的针脚
-SoftwareSerial
-softSerial(5, 6); //5为RX, 6为TX
+int input3 = 9; // 定义uno的pin 9 向 input3 输出
+int input4 = 10; // 定义uno的pin 10 向 input4 输出
+
 void setup() {
-  Serial.begin(9600); //设定硬串口波特率
-  softSerial.begin(9600); //设定软串口波特率
+       //  Serial.begin (9600);
+       //初始化各IO,模式为OUTPUT 输出模式
+       pinMode(input3,OUTPUT);
+       pinMode(input4,OUTPUT);
+ 
 }
+ 
 void loop() {
-  if (softSerial.available()) { //如果HC-06发来数据
-    int k = softSerial.read(); //读取1个字节的数据
-    Serial.println(k); //通过硬串口打印输出
-  }
+  //forward 向前转
+  digitalWrite(input3,HIGH); //给高电平
+  digitalWrite(input4,LOW);  //给低电平
+ 
 }
